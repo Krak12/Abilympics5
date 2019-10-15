dd6e7c2cb85841dd7630b58f9c8506bc50f051ba
Krak12 <mamukin08@gmail.com>
2019-10-15 11:45:59 +0300

    Save

--- Abilympics5/Abilympics5/MainWindow.xaml.cs
+++ Abilympics5/Abilympics5/MainWindow.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Abilympics5
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для WindowMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowMain : Window
     {
-        public MainWindow()
+        public WindowMain()
         {
             InitializeComponent();
         }
-
-        private void CheckBox1_Checked(object sender, RoutedEventArgs e)
-        {
-            passwordBox1.Visibility = Visibility.Visible;
-        }
-
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
     }
 }
