094d57e8b2fdb369188786134f0d164f275d20f3
Krak12 <mamukin08@gmail.com>
2019-10-16 22:26:20 +0500

    Change form

--- Abilympics5/Abilympics5/SpecialistMenu.xaml.cs
+++ Abilympics5/Abilympics5/SpecialistMenu.xaml.cs
@@ -27,6 +27,50 @@ namespace Abilympics5
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
+            Abilympics5.dbDataSet dbDataSet = ((Abilympics5.dbDataSet)(this.FindResource("dbDataSet")));
+            // Загрузить данные в таблицу Orders. Можно изменить этот код как требуется.
+            Abilympics5.dbDataSetTableAdapters.OrdersTableAdapter dbDataSetOrdersTableAdapter = new Abilympics5.dbDataSetTableAdapters.OrdersTableAdapter();
+            dbDataSetOrdersTableAdapter.Fill(dbDataSet.Orders);
+            System.Windows.Data.CollectionViewSource ordersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("ordersViewSource")));
+            ordersViewSource.View.MoveCurrentToFirst();
+            // Загрузить данные в таблицу Workers. Можно изменить этот код как требуется.
+            Abilympics5.dbDataSetTableAdapters.WorkersTableAdapter dbDataSetWorkersTableAdapter = new Abilympics5.dbDataSetTableAdapters.WorkersTableAdapter();
+            dbDataSetWorkersTableAdapter.Fill(dbDataSet.Workers);
+            System.Windows.Data.CollectionViewSource workersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("workersViewSource")));
+            workersViewSource.View.MoveCurrentToFirst();
+        }
+
+        // переходы по tabPage
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // закрытие формы
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // переход на форму ChangePassSp
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
